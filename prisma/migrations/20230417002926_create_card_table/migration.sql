55929d9af6e61a63b3fa2253111fb87cffa5b45c
AngeloVSO <angelo_odwyer@hotmail.com>
2023-04-16 23:40:29 -0300

    atualiza migration

--- prisma/migrations/20230417002926_create_card_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "cards" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "imageUrl" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "cards_name_key" ON "cards"("name");
