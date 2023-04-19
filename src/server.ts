da09500388493af2aa149f46def1be2b3bbbb817
AngeloVSO <angelo_odwyer@hotmail.com>
2023-04-18 21:56:59 -0300

    implementa rotas de login e register

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,28 @@
 import express from "express";
 import cors from "cors";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
 import { prismaClient } from "./prisma/client";
+import Jwt from "jsonwebtoken";
 
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/users", async (request, response) => {
-  const { email, username, name } = request.body;
+app.post("/auth/register", async (request, response) => {
+  const { email, username, name, password, confirmPassword, role } =
+    request.body;
+
+  if (!email || !name || !password || !username || !confirmPassword) {
+    return response
+      .status(422)
+      .json({ error: "Todos os campos são obrigatórios" });
+  }
+
+  if (password !== confirmPassword) {
+    return response.status(400).json({ error: "A senha precisa ser igual" });
+  }
 
   const verifyIfExistsUser = await prismaClient.user.findFirst({
     where: {
@@ -19,17 +34,61 @@ app.post("/users", async (request, response) => {
   });
 
   if (verifyIfExistsUser)
-    return response.status(400).json({ error: "User already exists" });
+    return response
+      .status(400)
+      .json({ error: "Usurário já cadastrado com esse nome ou email" });
+
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hash(password, salt);
 
   const userCreated = await prismaClient.user.create({
     data: {
-      email,
       username,
       name,
+      email,
+      password: passwordHash,
+      role: role ? process.env.ROLE_ADMIN! : "user",
     },
   });
 
   return response.json(userCreated);
+});
+
+app.get("/auth/login", async (request, response) => {
+  const { email, username, password } = request.body;
+
+  if (!email || !password || !username) {
+    return response.status(422).json({ error: "Credenciais inválidas" });
+  }
+
+  const user = await prismaClient.user.findFirst({
+    where: {
+      username,
+      OR: {
+        email,
+      },
+    },
+  });
+
+  if (!user)
+    return response.status(404).json({ error: "Usurário não encontrado" });
+
+  const checkPassword = await bcrypt.compare(password, user.password);
+
+  if (!checkPassword)
+    return response.status(400).json({ error: "Credenciais inválidas" });
+
+  const secret = process.env.SECRET!;
+  const token = Jwt.sign(
+    {
+      id: user.id,
+    },
+    secret
+  );
+
+  return response
+    .status(200)
+    .json({ success: "Usuário logado com sucesso!", token });
 });
 
 app.post("/cards", async (request, response) => {
